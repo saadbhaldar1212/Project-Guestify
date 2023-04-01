ba8ba23fa92f00ace4dae643b3e3307d526aa001
Mohammad Saad Bhaldar <saadbhaldar1212@gmail.com>
2023-04-01 18:25:40 +0530

    Update event_dashboard.dart

--- lib/dashboard/bottomappbar_pages/event_dashboard.dart
+++ lib/dashboard/bottomappbar_pages/event_dashboard.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_local_variable
 
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:guestify/utils/event_info/edit_event_info.dart';
 import 'package:guestify/utils/signout_button/signout_button.dart';
@@ -40,7 +41,7 @@ class _EventDashboardState extends State<EventDashboard> {
 
   @override
   Widget build(BuildContext context) {
-    final ref = db.child('events/');
+    final eventRef = db.child('events/');
     const pk = 'eventInfo';
 
     return Scaffold(
@@ -59,6 +60,102 @@ class _EventDashboardState extends State<EventDashboard> {
         ),
         actions: const [
           SignOutButton(),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(28.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'Events',
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.black,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Text(
+                  'Event Name',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  'Action',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: FirebaseAnimatedList(
+              query: eventRef,
+              shrinkWrap: true,
+              itemBuilder: (context, snapshot, animation, index) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '1) ${snapshot.child("Event Name").value.toString()}',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    // Text(
+                    //   "${snapshot.child('Event Date').value.toString()}, ${snapshot.child('Event Time').value.toString()}",
+                    //   style: const TextStyle(
+                    //     color: Colors.black,
+                    //     fontSize: 16,
+                    //   ),
+                    // ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          onPressed: (() {
+                            showModalBottomSheet(
+                              enableDrag: false,
+                              isDismissible: false,
+                              isScrollControlled: false,
+                              context: context,
+                              builder: (context) => const EditEventInfo(),
+                            );
+                          }),
+                          icon: const Icon(
+                            Icons.edit,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: (() {}),
+                          icon: const Icon(
+                            Icons.delete,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
         ],
       ),
       // body: const EditEventInfo(),

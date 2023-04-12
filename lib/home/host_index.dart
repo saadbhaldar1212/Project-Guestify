0f66dbe4c4bd8946420eea4083945eb393d3e5ea
Mohammad Saad Bhaldar <saadbhaldar1212@gmail.com>
2023-04-12 22:57:40 +0530

    host route

---
+++ lib/home/host_index.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HostIndex extends StatelessWidget {
+  const HostIndex({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/welcome/welcome2.dart
+++ lib/welcome/welcome2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guestify/home/host_index.dart';
 import 'package:guestify/login/admin_login.dart';
 import 'package:guestify/login/employee_login.dart';
 
@@ -164,7 +165,7 @@ class _Welcome2State extends State<Welcome2> {
                                         context,
                                         MaterialPageRoute(
                                           builder: (context) =>
-                                              const Welcome2(),
+                                              const HostIndex(),
                                         ));
                                   }),
                                   child: const Text(
